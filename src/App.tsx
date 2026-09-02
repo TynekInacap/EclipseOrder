46fc5439ad91cffc97cb5cb418aff1bb937ceea0
Eclipse Order Developer <dev@eclipseorder.com>
2026-09-02 11:23:35 -0600

    Bloquea acceso hasta validar OTP

--- src/App.tsx
+++ src/App.tsx
@@ -6461,7 +6461,7 @@ export default function App() {
         setView("login")
       } else if (isLegacyAuthEmail(session.user.email) && _event !== "USER_UPDATED") {
         setView("link_email")
-      } else if (!currentUserRef.current || currentUserRef.current.id !== session.user.id) {
+      } else if (_event !== "USER_UPDATED" && (!currentUserRef.current || currentUserRef.current.id !== session.user.id)) {
         void hydrateSession(session.user.id).catch((sessionError) => {
           console.error("Could not load Supabase profile", sessionError)
         })
